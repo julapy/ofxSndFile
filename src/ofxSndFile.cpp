72b913e8ff94309feccbda0cb78cb6030208390b
lukasz.karluk <julapy@googlemail.com>
2011-10-30 20:10:54 +1100

    first commit

---
+++ src/ofxSndFile.cpp
@@ -0,0 +1,19 @@
+//
+//  ofxSndFile.cpp
+//  emptyExample
+//
+//  Created by lukasz karluk on 15/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include "ofxSndFile.h"
+
+ofxSndFile :: ofxSndFile ()
+{
+    //
+}
+
+ofxSndFile :: ~ofxSndFile ()
+{
+    //
+}
---
+++ src/ofxSndFile.h
@@ -0,0 +1,20 @@
+//
+//  ofxSndFile.h
+//  emptyExample
+//
+//  Created by lukasz karluk on 15/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#pragma once
+
+#include "ofMain.h"
+#include <sndfile.hh>
+
+class ofxSndFile
+{
+public:
+     ofxSndFile ();
+    ~ofxSndFile ();
+    
+};
